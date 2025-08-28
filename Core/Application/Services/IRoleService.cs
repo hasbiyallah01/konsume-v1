e860bcfce61a7966fdad86b384bec231848bf6c9
hasbiyallah01 <oyebohm@gmail.com>
2025-08-28 22:09:05 +0100

    Add IRoleService interface and RoleRequest model for role management

---
+++ Core/Application/Services/IRoleService.cs
@@ -0,0 +1,12 @@
+﻿using konsume_v1.Models;
+using konsume_v1.Models.RoleModel;
+
+namespace konsume_v1.Core.Application.Services
+{
+    public interface IRoleService
+    {
+        Task<BaseResponse> CreateRole(RoleRequest request);
+        
+    }
+
+}
---
+++ Models/RoleModel/RoleRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace konsume_v1.Models.RoleModel
+{
+    public class RoleRequest
+    {
+        public string Name { get; set; } = default!;
+        public string? Description { get; set; }
+    }
+}
