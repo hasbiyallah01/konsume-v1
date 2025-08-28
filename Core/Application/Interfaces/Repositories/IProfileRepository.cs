c5463650217a8389b3d548405b53d0a2633c89cf
hasbiyallah01 <oyebohm@gmail.com>
2025-08-28 22:07:34 +0100

    Add IProfileRepository and IVerificationCodeService interfaces, along with BaseResponse and VerificationCodeDto models

--- Core/Application/Interfaces/Repositories/IProfileRepository.cs
+++ Core/Application/Interfaces/Repositories/IProfileRepository.cs
@@ -1,4 +1,5 @@
 ﻿using konsume_v1.Core.Domain.Entities;
+using System.Linq.Expressions;
 
 namespace konsume_v1.Core.Application.Interfaces.Repositories
 {
@@ -9,6 +10,14 @@ namespace konsume_v1.Core.Application.Interfaces.Repositories
         Task<bool> GetProfileByUserIdAsync(int id);
         Task<int> GetProfileDetailsByUserIdAsync(int id);
         Task<Profile> GetAsync(string email);
+        Task<Profile> GetAsync(Expression<Func<Profile, bool>> exp);
+        Task<ICollection<Profile>> GetAllAsync();
+        void Remove(Profile Profile);
+        Profile Update(Profile Profile);
+        Task<bool> ExistsAsync(string email, int id);
+        Task<bool> ExistsAsync(string email);
     }
 
 }
+
+
---
+++ Models/VerificationCodeDto.cs
@@ -0,0 +1,13 @@
+﻿namespace konsume_v1.Models
+{
+    public class VerificationCodeDto
+    {
+        public string Code { get; set; }
+        public bool isExpired { get; set; }
+    }
+
+    public class ResetPasswordRequestModel
+    {
+        public string Email { get; set; }
+    }
+}
