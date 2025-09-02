ba6f5af4c300422ef3618e6c4b139b7c8739b7a1
hasbiyallah01 <oyebohm@gmail.com>
2025-09-02 19:36:20 +0100

    Add UserInteraction entity to represent user questions and responses

---
+++ Core/Domain/Entities/UserInteraction.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace konsume_v1.Core.Domain.Entities
+{
+    public class UserInteraction :Auditables
+    {
+        public int UserId { get; set; }
+        public virtual User User { get; set; } = default!;
+        public string Question { get; set; }
+        public string Response { get; set; }
+    }
+
+}
